eed842d298c5cf1fded3d36282d23f9e7f1fcdad
Sridhar Periasami <sridhar.periasami@gmail.com>
2015-10-24 01:35:16 -0400

    Setting removed

--- Internationalcall/app/src/main/java/com/example/root/internationalcall/NumberPadActivity.java
+++ Internationalcall/app/src/main/java/com/example/root/internationalcall/NumberPadActivity.java
@@ -30,7 +30,7 @@ public class NumberPadActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_number_pad, menu);
+        //getMenuInflater().inflate(R.menu.menu_number_pad, menu);
         return true;
     }
 
